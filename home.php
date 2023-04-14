b7d2a1a6f6cf29ebc28b00b7177b5bb3c82e0f44
HabibTriki <moahmedhabib.triki@insat.ucar.tn>
2023-04-14 12:46:32 +0100

    from DB to home

--- home.php
+++ home.php
@@ -1,36 +1,52 @@
 <?php
 $pageTitle = 'Recipes | Home';
 include 'fragments/header.php';
-if (isset($_GET['success'])) {
-    echo '<p>Recipe added successfully!</p>';
-}
 ?>
 
-<ul class="recipes">
-    <a href="details.php">
-    <li class="recipe">
-        <img src="uploads/recipe1.jpeg" alt="recipe img">
-        <h4>Makarouna bel djej</h4>
-        <p>By Aymen</p>
-    </li></a>
+<!--<ul class="recipes">-->
+<!--    <a href="details.php">-->
+<!--    <li class="recipe">-->
+<!--        <img src="uploads/recipe1.jpeg" alt="recipe img">-->
+<!--        <h4>Makarouna bel djej</h4>-->
+<!--        <p>By Aymen</p>-->
+<!--    </li></a>-->
+<!---->
+<!--    <a href="details.php">-->
+<!--    <li class="recipe">-->
+<!--        <img src="uploads/recipe2.jpeg" alt="recipe img">-->
+<!--        <h4>Koskssi bl l7am </h4>-->
+<!--        <p>By Hbib</p>-->
+<!--    </li></a>-->
+<!---->
+<!--    <a href="details.php">-->
+<!--    <li class="recipe">-->
+<!--        <img src="uploads/recipe3.jpg" alt="recipe img">-->
+<!--        <h4>Kamounia</h4>-->
+<!--        <p>By Ala</p>-->
+<!--    </li></a>-->
 
-    <a href="details.php">
-    <li class="recipe">
-        <img src="uploads/recipe2.jpeg" alt="recipe img">
-        <h4>Koskssi bl l7am </h4>
-        <p>By Hbib</p>
-    </li></a>
+    <?php
+    require_once ('RecipesRepository.php');
+    $rep= new RecipesRepository("recipes");
+    $recipes = $rep->findAll();
+    ?>
+    <ul class="recipes">
+    <?php
+        foreach ($recipes as $recipe) {
+        $nom=$recipe->nom;
+        $author=$recipe->author;
+        $image=$recipe->image;
+        $dataUri = 'data:image/jpeg;base64,' . base64_encode($image);
+        echo '<a href="details.php">'.
+         '<li class="recipe">'.
+            '<img src="' . $dataUri .'" height ="300" width="300" '. '" alt="recipe img">'.
+         '<h4>' . $nom . '<h4>'.
+         '<p>By ' . $author . '</p>'.
+         '</li></a>';
+    }
+    ?>
 
-    <a href="details.php">
-    <li class="recipe">
-        <img src="uploads/recipe3.jpg" alt="recipe img">
-        <h4>Kamounia</h4>
-        <p>By Ala</p>
-    </li></a>
-
-</ul>
-
+    </ul>
 <?php
 include 'fragments/footer.php';
 ?>
-
