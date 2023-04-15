13e06d6f43c6afee23995f9792775adb7b842199
HabibTriki <moahmedhabib.triki@insat.ucar.tn>
2023-04-15 12:05:31 +0100

    jpeg

--- update.php
+++ update.php
@@ -29,7 +29,7 @@ $DataUri='data:image/jpeg;base64,' . base64_encode($image)
     <label for="image">Image</label>
     <img src="<?= $DataUri ?>" height="100" width="100" alt="recipe image">
     <input id="image" type="file" name="my_image" required>
-    <small class="form-text text-muted">Choose a JPG or PNG image under 5 MB.</small>
+    <small class="form-text text-muted">Choose a JPG , JPEG or PNG image under 5 MB.</small>
 
     <label for="rating">Rating</label>
     <input id="rating"  type="number" name="rating" value="<?=$recipe->rating?>" required>
