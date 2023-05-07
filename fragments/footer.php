02cc54b0c5602a4d113129b4841d7806373bba99
Yessine Ellini <yessine.ellini>
2023-05-07 22:55:53 +0100

    update the footer include the website source code's license as well as the recipes' and images' license

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,4 +1,7 @@
-<footer>Copyright &copy; Team 2023</footer>
+<footer>
+<p>The recipes and images are <a href="https://creativecommons.org/licenses/by-sa/4.0/">cc-by-sa</a>. The <a href="https://github.com/AymenKoched/Projet-PHP">code</a> is <a href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL-3.0+</a></p>
+<!-- Either make the repo public or change the above link to a public mirror -->
+</footer>
 
 </body>
 </html>
