7fb835e63cf4e9a424635f5f9b976e8f31e5f202
Yessine Ellini <yessine.ellini>
2023-05-10 17:33:22 +0100

    Revert "edit footer"
    This reverts commit d9d9164c774c4892a10bb319b3da4132a00e6d26.
    It makes the footer too big (more than half the screen space on my
    laptop), has the wrong license, and contrasts with the main background
    color
    See
    https://github.com/AymenKoched/Projet-PHP/commit/d9d9164c774c4892a10bb319b3da4132a00e6d26#commitcomment-112744293
    for screenshots of old vs new

--- fragments/footer.php
+++ fragments/footer.php
@@ -1,14 +1,6 @@
 <footer>
-<!--<p>The recipes and images are <a href="https://creativecommons.org/licenses/by-sa/4.0/">cc-by-sa</a>. The <a href="https://github.com/AymenKoched/Projet-PHP">code</a> is <a href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL-3.0+</a></p>-->
+<p>The recipes and images are <a href="https://creativecommons.org/licenses/by-sa/4.0/">cc-by-sa</a>. The <a href="https://github.com/AymenKoched/Projet-PHP">code</a> is <a href="https://www.gnu.org/licenses/agpl-3.0.txt">AGPL-3.0+</a></p>
 <!-- Either make the repo public or change the above link to a public mirror -->
-        <img class="logo" src="\uploads\logo-removebg-preview.png" alt="HarissaHouse logo">
-
-        <div class="contact">
-            <span><strong>Address:</strong> 676 INSAT Centre Urbain Nord BP، Tunis Cedex, 1080</span>
-            <span><strong>Phone:</strong> (+216) 78 759 738 </span>
-            <span><strong>Email:</strong> info@harissahouse.com</span>
-        </div>
-    <p>&copy; HarissaHouse 2023. All rights reserved.</p>
 </footer>
 
 <?php
