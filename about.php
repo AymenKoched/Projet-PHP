2d0ba3e34259bcfdab412a565cb6cfabdf18749b
ala eddine achach <alaeddine.achach@insat.ucar.tn>
2023-05-10 23:54:14 +0100

    about reglation

--- about.php
+++ about.php
@@ -13,81 +13,80 @@ include 'fragments/header.php';
 <div class="team-container">
     <div class="member 1">
         <div class="card front-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img.png" alt="Flip Card">
         </div>
         <div class="card back-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img.png" alt="Flip Card">
             <div class="info">
                 <div class="title">Mohamed Habib Triki</div>
                 <p>Software engineering student</p>
             </div>
             <ul>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/Haboubtriki7"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/habib_triki/"><i class="fab fa-instagram"></i></a>
             </ul>
         </div>
     </div>
     <div class="member 2">
         <div class="card front-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_1.png" alt="Flip Card">
         </div>
         <div class="card back-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_1.png" alt="Flip Card">
             <div class="info">
-                <div class="title">Mohamed Habib Triki</div>
+                <div class="title">Aymen Koched</div>
                 <p>Software engineering student</p>
             </div>
             <ul>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/Aymen.Koched.2002"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/aymenkoched/"><i class="fab fa-instagram"></i></a>
             </ul>
         </div>
     </div>
     <div class="member 3">
         <div class="card front-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors\IMG_1692.jpg" alt="Flip Card">
         </div>
         <div class="card back-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors\IMG_1692.jpg" alt="Flip Card">
             <div class="info">
-                <div class="title">Mohamed Habib Triki</div>
+                <div class="title">Ala Eddine Achach</div>
                 <p>Software engineering student</p>
             </div>
             <ul>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100008310468991"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/alaeddineachach/"><i class="fab fa-instagram"></i></a>
             </ul>
         </div>
     </div>
     <div class="member 4">
         <div class="card front-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_2.png" alt="Flip Card">
         </div>
         <div class="card back-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_2.png" alt="Flip Card">
             <div class="info">
-                <div class="title">Mohamed Habib Triki</div>
+                <div class="title">Mohamed Yessine Ellini </div>
                 <p>Software engineering student</p>
             </div>
             <ul>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="mohamed yassine.ellini@insat.ucar.tn"><i class="fab fa-google"></i></a>
             </ul>
         </div>
     </div>
     <div class="member 5">
         <div class="card front-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_3.png" alt="Flip Card">
         </div>
         <div class="card back-face">
-            <img src="https://scontent.ftun9-1.fna.fbcdn.net/v/t39.30808-6/342822986_226127490105071_3781577970052647618_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lv5coebeC8gAX_w1QOK&_nc_ht=scontent.ftun9-1.fna&oh=00_AfDiwg2geQe9yfMK1YuMLvR3ABdwPt2Rdu8Df5xwpwW_Ag&oe=645935DE" alt="Flip Card">
+            <img src="contributors/img_3.png" alt="Flip Card">
             <div class="info">
-                <div class="title">Mohamed Habib Triki</div>
+                <div class="title">Mohamed Zini</div>
                 <p>Software engineering student</p>
             </div>
             <ul>
-                <a href="#"><i class="fab fa-facebook-f"></i></a>
-                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/profile.php?id=100010182674973&sk=about"><i class="fab fa-facebook-f"></i></a>
+                <a href="https://www.instagram.com/ezinimohamed/"><i class="fab fa-instagram"></i></a>
             </ul>
         </div>
     </div>
