55c7e60eaab031682adab590b0243b8d2674c4d0
Yessine Ellini <yessine.ellini>
2023-05-12 23:12:08 +0100

    move the database connection credentials to a separate config file

--- database_access/ConnexionPDO.php
+++ database_access/ConnexionPDO.php
@@ -1,18 +1,25 @@
 <?php
+set_include_path(get_include_path() . PATH_SEPARATOR . $_SERVER['DOCUMENT_ROOT']);
 
 class ConnexionPDO
 {
-    private static $_dbname = "projetphp";
-    private static $_user = "root";
-    private static $_pwd = "";
-    private static $_host = "localhost";
+    private $_dbname;
+    private $_user;
+    private $_password;
+    private $_host;
     private static $_bdd = null;
 
     private function __construct()
     {
+        $config = parse_ini_file('config.ini');
+        $this->_dbname = $config['db_name'];
+        $this->_user = $config['db_user'];
+        $this->_password = $config['db_password'];
+        $this->_host = $config['db_host'];
+
         try {
-            self::$_bdd = new PDO("mysql:host=" . self::$_host . ";dbname="
-                .self::$_dbname . ";charset=utf8", self::$_user, self::$_pwd,
+            self::$_bdd = new PDO("mysql:host=" . $this->_host . ";dbname=".
+                $this->_dbname . ";charset=utf8", $this->_user, $this->_password,
                 array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         } catch (PDOException $e) {
             die('Erreur : ' . $e->getMessage());
